6983232a0f2a9d028044d9ad48680b7fd3bdf465
Guilherme Salvi Fernandes <guilhermesalvi.dev@outlook.com>
2020-07-09 22:20:07 -0300

    Add project files.

---
+++ MultiLanguageApplication/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using MultiLanguageApplication.Models;
+using System;
+
+namespace MultiLanguageApplication.Controllers
+{
+    [ApiController]
+    [Route("api/student")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStringLocalizer<StudentController> _localizer;
+
+        public StudentController(IStringLocalizer<StudentController> localizer)
+        {
+            this._localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
+        }
+
+        [HttpPost("")]
+        public IActionResult Register(StudentInputModel model)
+        {
+            return Ok(new
+            {
+                Succeeded = true,
+                Message = this._localizer["Student_Registered"].Value
+            });
+        }
+    }
+}
